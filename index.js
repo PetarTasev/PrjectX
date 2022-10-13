ff64578349faeca2e1d53db17ccc31e1c302d05f
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-10-13 20:39:29 +0300

    ca comence a prenser forme

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const hbr = require('express-handlebars')
+
+const handlebars = hbr.create({
+    extname: '.hbs'
+})
+
+const app = express()
+
+app.engine('.hbr', handlebars.engine)
+app.set('view engine', '.hbs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
