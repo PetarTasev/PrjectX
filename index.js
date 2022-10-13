843098dedc0ce93a9332151fedfc53371e9fb99a
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-10-13 20:47:49 +0300

    commit

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ app.engine('.hbr', handlebars.engine)
 app.set('view engine', '.hbs')
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {
+        title: 'Handlebars Demo'
+    })
 })
+
+app.listen(3000)
